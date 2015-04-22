5bb80e4a720b7451fc3727287b8e6cd97c498ee9
charmyin <976582265@qq.com>
2015-04-23 00:14:51 +0800

    Camera config on dev~

--- routes/camera.js
+++ routes/camera.js
@@ -8,33 +8,57 @@ var db = new sqlite3.Database('cameraconfig.db');
 
 /* GET users listing. */
 router.get('/show', function(req, res) {
-  res.render('config',{});
-});
-
-
-/* GET users listing. */
-router.post('/save', function(req, res) {
-
 
   db.serialize(function() {
-    db.run("CREATE TABLE lorem (info TEXT)");
-
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-        console.log(row.id + ": " + row.info);
+   // db.run("CREATE TABLE camera_config (info TEXT,)");
+    db.get("SELECT * FROM camera_config", function(err, row) {
+        //如果不存在，save
+        if(err || !row){
+          res.json({result:"请先初始化表！"});
+          return;
+        }
+        //update
+        if(row && row.id==1){
+          res.render("config",row);
+        }
     });
   });
 
 
+ // res.json({result:"未知原因错误！"});
+});
 
 
+/* 保存相机配置 */
+router.post('/save', function(req, res) {
+  db.serialize(function() {
+   // db.run("CREATE TABLE camera_config (info TEXT,)");
+    db.get("SELECT * FROM camera_config", function(err, row) {
+        //如果不存在，save
+        if(err || !row){
+          res.json({result:"请先初始化表！"});
+        }
+        //update
+        if(row && row.id==1){
+          var stmt = db.prepare("UPDATE camera_config SET localCircle=(?), uploadCircle=(?), localResolution=(?), uploadResolution=(?), isRfid=(?), isClosed=(?)");
+          stmt.run(req.body.localCircle, req.body.uploadCircle, req.body.localResolution, req.body.uploadResolution, req.body.isRfid, req.body.isClosed);
+          console.log(req.body);
+        }
+    });
+  });
 
   res.redirect('/camera/show');
+});
+
+/* GET users listing. */
+router.get('/initdb', function(req, res) {
+  db.serialize(function() {
+    db.run("DROP TABLE IF EXISTS camera_config");
+    db.run("CREATE TABLE camera_config (id INTEGER, localCircle INTEGER, uploadCircle INTEGER, localResolution TEXT, uploadResolution TEXT, isRfid INTEGER, isClosed INTEGER )");
+    db.run("INSERT INTO camera_config VALUES (1,30, 10, 1920*1080, 1024*768, 1, 1)");
+  });
+
+  res.json({ok:true});
 });
 
 module.exports = router;
